3f8df89f7f23dcfd1c6d882ddfe8c25d3138bf29
Jason Seawall <jseawall@numurus.com>
2025-11-08 11:36:38 -0800

    **

--- scripts/docker_env_setup.sh
+++ scripts/docker_env_setup.sh
@@ -23,8 +23,8 @@ echo "Running Intitialization Scripts"
 
 export CONFIG_USER=$(id -un 1000)
 
-if [[ ! {$CONFIG_USER} == 'nepihost' ]]; then
-   echo 'This scripts must be run as nepihost user.'
+if [[ {$CONFIG_USER} != 'nepihost' ]]; then
+   echo "Current user is ${CONFIG_USER}. This scripts must be run as nepihost user."
    exit 1
 fi
 
