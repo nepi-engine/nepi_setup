467c198b42672f1932c23e7f731b527af6a9a0cb
Jason Seawall <jseawall@numurus.com>
2025-11-08 11:35:28 -0800

    **

--- scripts/nepi_env_setup.sh
+++ scripts/nepi_env_setup.sh
@@ -17,7 +17,7 @@ sudo -v
 export CONFIG_USER=$(id -un 1000)
 
 if [[ ! {$CONFIG_USER} == 'nepi' ]]; then
-   echo 'This scripts must be run as nepihost user.'
+   echo "Current user is ${CONFIG_USER}. This scripts must be run as nepihost user."
    exit 1
 fi
 
