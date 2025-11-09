fc67a0cea190a031392d1225c6da1fc75bb737b4
Jason Seawall <jseawall@numurus.com>
2025-11-09 08:10:03 -0800

    **

--- scripts/docker_user_setup.sh
+++ scripts/docker_user_setup.sh
@@ -51,7 +51,7 @@ else
     #sudo useradd -m -s /bin/bash -p "$(openssl passwd -1 ${CONFIG_USER_PW})" ${CONFIG_USER}
     #sudo useradd $CONFIG_USER -s /bin/bash -g sudo -m
     sudo adduser --gecos "New User Account" --disabled-password "$CONFIG_USER"
-    
+    echo "${CONFIG_USER}:${CONFIG_USER_PW}" | sudo chpasswd
 fi    
 if id -u "$CONFIG_USER" >/dev/null 2>&1; then
     echo "Configuring NEPI Base User account $CONFIG_USER"
