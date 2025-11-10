8565f915d6f0bbbdd986e1fbd4d62a73ae73f3bb
Jason Seawall <jseawall@numurus.com>
2025-11-10 14:58:07 -0800

    **

--- scripts/nepi_config_setup.sh
+++ scripts/nepi_config_setup.sh
@@ -261,6 +261,8 @@ echo ""
 echo "Running Sync to Configs Script"
 source /opt/nepi/etc/scripts/sync_to_configs.sh
 
+exit 1
+
 #######################################################################################
 
 
