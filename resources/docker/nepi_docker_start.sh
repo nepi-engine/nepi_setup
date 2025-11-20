1773872c6cbd1e441de63864ff51fd299442cb5c
Jason Seawall <jseawall@numurus.com>
2025-11-20 11:18:11 -0800

    **

--- resources/docker/nepi_docker_start.sh
+++ resources/docker/nepi_docker_start.sh
@@ -12,214 +12,185 @@
 # This script launches NEPI Container
 sudo -v
 
-CONFIG_USER=nepihost
+CONFIG_USER=$(id -un)
 source /home/${CONFIG_USER}/.nepi_bash_utils
 wait
 
 DOCKER_FOLDER=$(cd -P "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 
-
-########################
-# Update Docker Config
-#echo ""
-echo "Updating Docker Config File"
-bash ${DOCKER_FOLDER}/nepi_docker_update.sh
+echo "Calling NEPI Docker Stop Process"
+source ${DOCKER_FOLDER}/nepi_docker_stop.sh
 wait
 
+
+# Reset. Updated by NEPI Software
+SYSTEM_CONFIG_FILE=/mnt/nepi_config/system_cfg/etc/nepi_system_config.yaml
+update_yaml_value "NEPI_VERSION" 'XpXpX' "$SYSTEM_CONFIG_FILE"
+update_yaml_value "NEPI_SW_DESC" 'unknown' "$SYSTEM_CONFIG_FILE"
+update_yaml_value "NEPI_FS_AB" $NEPI_AB_FS "$SYSTEM_CONFIG_FILE"
+
+
+if [[ "$NEPI_ACTIVE_FS" == "nepi_fs_a" ]]; then
+    nepi_fs=${NEPI_FSA}
+    nepi_fs_tag=${NEPI_FSA_TAG}
+else
+    nepi_fs=${NEPI_FSB}
+    nepi_fs_tag=${NEPI_FSB_TAG}
+fi
+
+
+echo ""
+echo "Cleaning and Fixing Folders"
+sudo rm -r /tmp/*
+sudo chown ${CONFIG_USER}:${CONFIG_USER} /mnt/nepi_config
+sudo chown ${CONFIG_USER}:${CONFIG_USER} /mnt/nepi_storage
+echo ""
+
+
+
+### This is done by nepi_docker service before calling nepi_docker_start
+#######################
+# Update ETC Config Files
+#######################
+# source ${DOCKER_FOLDER}/update_etc_files.sh
+# wait
+
 ########################
-# Load NEPI DOCKER
+# Build Run Command
+########################
+echo "Building NEPI Docker Run Command"
 
-DOCKER_CONFIG_FILE=${DOCKER_FOLDER}/nepi_docker_config.yaml
+# NEPI_STORAGE=/mnt/nepi_storage
+# NEPI_CONFIG=/mnt/nepi_coinfig
 
-echo "Loading Docker Config File"
-source ${DOCKER_FOLDER}/load_docker_config.sh
-if [[ "$?" -eq 1 ]]; then
-    echo "Failed to load ${DOCKER_CONFIG_FILE}"
+# if [[ "$1" -eq 0 ]]; then 
+#     echo "Starting with rm process disabled"
+#     rm_cmd=''
+# if [[ "$1" -eq 1 ]]; then 
+#     echo "Starting with rm process enabled"
+#     rm_cmd="--rm"
+# elif [[ "$NEPI_RM_PS" -eq 0 ]]; then
+#     echo "Starting with rm process disabled"
+#     rm_cmd=''
+# else
+#     echo "Starting with rm process enabled"
+#     rm_cmd="--rm"
+# fi
+
+rm_cmd="--rm"
+
+
+########
+# Initialize Run Command
+DOCKER_RUN_COMMAND="sudo docker run -d --privileged ${rm_cmd} -e UDEV=1 --ipc=host --user 0:0 \
+--mount type=bind,source=/mnt/nepi_storage,target=/mnt/nepi_storage \
+--mount type=bind,source=/mnt/nepi_config,target=/mnt/nepi_config \
+--mount type=bind,source=/dev,target=/dev \
+--cap-add=SYS_TIME --volume=/var/empty:/var/empty -v /etc/ntpd.conf:/etc/ntpd.conf \
+-e DISPLAY=$DISPLAY \
+--net=host \
+-p 2222:22 \
+-p 9091:9091 \
+-p 9092:9092 \
+-p 11311:11311 \
+--name samba-server \
+-p 137:137/udp \
+-p 138:138/udp \
+-p 139:139/tcp \
+-p 445:445/tcp "
+
+# Set cuda support if needed
+
+if is_valid_cuda; then
+    echo "Enabling CUDA GPU Support TRUE"
+DOCKER_RUN_COMMAND="${DOCKER_RUN_COMMAND} \
+--gpus all \
+--runtime nvidia \
+-v /tmp/.X11-unix/:/tmp/.X11-unix "
+
+fi 
+
+# Finish Run Command
+
+echo "Using name:tag ${nepi_fs}:${nepi_fs_tag} with Command"
+
+DOCKER_RUN_COMMAND="${DOCKER_RUN_COMMAND} \
+${nepi_fs}:${nepi_fs_tag} /bin/bash \
+-c 'service supervisor start'"
+
+#-c '/nepi_start_all'"
+
+#-c 'service supervisor start'"
+
+########################
+# Run NEPI Docker
+########################
+
+function dcheck() {
+    dname=$1
+    #dtag=$(sudo docker ps --format '{{.Image}}' | grep ${dname} | awk -F ':' '{print $1}' )
+    echo $(sudo docker ps -a)
+    did=$(sudo docker container ls  | grep $dname | awk '{print $1}')
+    if [[ -z "$did" ]]; then
+        return 1      
+    fi
+    return 0
+}
+
+
+
+echo ""
+echo "Launching NEPI Docker Container ${nepi_fs}:${nepi_fs_tag} with Command"
+echo "${DOCKER_RUN_COMMAND}"
+
+eval "$DOCKER_RUN_COMMAND"
+
+sleep 2
+
+CONTAINER_ID=($(sudo docker ps -qf "ancestor=${nepi_fs}:${nepi_fs_tag}"))
+CONTAINER_ID=${CONTAINER_ID[0]}
+
+# fail_count=0
+# retries=$NEPI_RETRY_COUNT
+# while [[ "$fail_count" -le "$NEPI_RETRY_COUNT" ]]; do
+#     ((fail_count++))
+#     eval "$DOCKER_RUN_COMMAND"
+
+#     sleep 3
+
+#     CONTAINER_ID=($(sudo docker ps -qf "ancestor=${nepi_fs}:${nepi_fs_tag}"))
+#     CONTAINER_ID=${CONTAINER_ID[0]}
+#     if [[ -n "$CONTAINER_ID" ]]; then
+#         break
+#     elif [[ "$fail_count" -le "$NEPI_RETRY_COUNT" ]]; then
+#         retries=$(( $retries -1 ))
+#         echo "NEPI Container Failed to Run. Will try again ${retries} times"
+#     fi
+# done
+
+if [[ -z "$CONTAINER_ID" ]]; then
+
+    echo "--------------------------"
+    echo "NEPI Container Failed to Run"
+    echo "--------------------------"   
 
 else
 
-    ########################
-    # Start Processes
-
-    # Reset. Updated by NEPI Software
-    SYSTEM_CONFIG_FILE=/mnt/nepi_config/system_cfg/etc/nepi_system_config.yaml
-    update_yaml_value "NEPI_VERSION" 'XpXpX' "$SYSTEM_CONFIG_FILE"
-    update_yaml_value "NEPI_SW_DESC" 'unknown' "$SYSTEM_CONFIG_FILE"
-    update_yaml_value "NEPI_FS_AB" $NEPI_AB_FS "$SYSTEM_CONFIG_FILE"
-
-
-    if [[ "$NEPI_ACTIVE_FS" == "nepi_fs_a" ]]; then
-        nepi_fs=${NEPI_FSA}
-        nepi_fs_tag=${NEPI_FSA_TAG}
-    else
-        nepi_fs=${NEPI_FSB}
-        nepi_fs_tag=${NEPI_FSB_TAG}
-    fi
-   
-    ########################NEPI_ACTIVE_FS
-
-
-
-    # Stop Any Running NEPI Containers
-    echo "Calling NEPI Docker Stop Process"
-    bash ${DOCKER_FOLDER}/nepi_docker_stop.sh
-    wait
-
-    ### This is done by nepi_docker service before calling nepi_docker_start
-    #######################
-    # Update ETC Config Files
-    #######################
-    # source ${DOCKER_FOLDER}/update_etc_files.sh
-    # wait
-
-    ########################
-    # Build Run Command
-    ########################
-    echo "Building NEPI Docker Run Command"
-
-    # NEPI_STORAGE=/mnt/nepi_storage
-    # NEPI_CONFIG=/mnt/nepi_coinfig
-    
-    # if [[ "$1" -eq 0 ]]; then 
-    #     echo "Starting with rm process disabled"
-    #     rm_cmd=''
-    # if [[ "$1" -eq 1 ]]; then 
-    #     echo "Starting with rm process enabled"
-    #     rm_cmd="--rm"
-    # elif [[ "$NEPI_RM_PS" -eq 0 ]]; then
-    #     echo "Starting with rm process disabled"
-    #     rm_cmd=''
-    # else
-    #     echo "Starting with rm process enabled"
-    #     rm_cmd="--rm"
-    # fi
-    
-    rm_cmd="--rm"
-
-
-    ########
-    # Initialize Run Command
-    DOCKER_RUN_COMMAND="sudo docker run -d --privileged ${rm_cmd} -e UDEV=1 --ipc=host --user 0:0 \
-    --mount type=bind,source=/mnt/nepi_storage,target=/mnt/nepi_storage \
-    --mount type=bind,source=/mnt/nepi_config,target=/mnt/nepi_config \
-    --mount type=bind,source=/dev,target=/dev \
-    --cap-add=SYS_TIME --volume=/var/empty:/var/empty -v /etc/ntpd.conf:/etc/ntpd.conf \
-    -e DISPLAY=$DISPLAY \
-    --net=host \
-    -p 2222:22 \
-    -p 9091:9091 \
-    -p 9092:9092 \
-    -p 11311:11311 \
-    --name samba-server \
-    -p 137:137/udp \
-    -p 138:138/udp \
-    -p 139:139/tcp \
-    -p 445:445/tcp "
-
-    # Set cuda support if needed
-
-    if is_valid_cuda; then
-        echo "Enabling CUDA GPU Support TRUE"
-    DOCKER_RUN_COMMAND="${DOCKER_RUN_COMMAND} \
-    --gpus all \
-    --runtime nvidia \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix "
-
-    fi 
-
-    # Finish Run Command
-
-    echo "Using name:tag ${nepi_fs}:${nepi_fs_tag} with Command"
-
-    DOCKER_RUN_COMMAND="${DOCKER_RUN_COMMAND} \
-    ${nepi_fs}:${nepi_fs_tag} /bin/bash \
-    -c '/nepi_start_all'"
-
-    #-c 'service supervisor start'"
-
-    ########################
-    # Run NEPI Docker
-    ########################
-
-    function dcheck() {
-        dname=$1
-        #dtag=$(sudo docker ps --format '{{.Image}}' | grep ${dname} | awk -F ':' '{print $1}' )
-        echo $(sudo docker ps -a)
-        did=$(sudo docker container ls  | grep $dname | awk '{print $1}')
-        if [[ -z "$did" ]]; then
-            return 1      
-        fi
-        return 0
-    }
-
-
-
     echo ""
-    echo "Launching NEPI Docker Container ${nepi_fs}:${nepi_fs_tag} with Command"
-    echo "${DOCKER_RUN_COMMAND}"
+    dps
+    echo ""
+    echo "--------------------------"
+    echo "NEPI Container Running with ID: ${CONTAINER_ID}"
+    echo "--------------------------"   
+    echo ""
 
-
-    fail_count=0
-    while [[ "$fail_count" -le "$NEPI_RETRY_COUNT" ]]
-
-        eval "$DOCKER_RUN_COMMAND"
-
-        # sleep 2
-        # dcheck $nepi_fs
-        # if [[ "$?" -eq 1 ]]; then
-        #     echo "NEPI Container ${dname} FAILED to run"
-        #     exit 1
-        # fi
-
-        # sleep 2
-        # dcheck $nepi_fs
-        # if [[ "$?" -eq 1 ]]; then
-        #     echo "Retrying NEPI Docker Container ${nepi_fs}:${nepi_fs_tag}"
-        #     eval "$DOCKER_RUN_COMMAND"
-        #     sleep 2
-        #     dcheck $nepi_fs
-        #     if [[ "$?" -eq 1 ]]; then
-        #         echo "NEPI Container ${dname} FAILED to run"
-        #         exit 1
-        #     fi
-        # fi
-
-
-        CONTAINER_ID=($(sudo docker ps -qf "ancestor=${nepi_fs}:${nepi_fs_tag}"))
-        CONTAINER_ID=${CONTAINER_ID[0]}
-        if [[ -z "$CONTAINER_ID" ]]; then
-            echo "NEPI Failed to Run"
-            CONTAINER_ID=($(sudo docker ps -aqf "ancestor=${nepi_fs}:${nepi_fs_tag}"))
-            if [[ -n "$CONTAINER_ID" ]]; then
-                echo "Removing stopped container ${nepi_fs}:${nepi_fs_tag} with id ${CONTAINER_ID}"
-                sudo docker rm $CONTAINER_ID
-            fi
-        fi
-
-    if [[ -z "$CONTAINER_ID" ]]; then
-
-        echo "--------------------------"
-        echo "NEPI Container Failed to Run"
-        echo "--------------------------"   
-
-    else
-
-        echo ""
-        dps
-        echo ""
-        echo "--------------------------"
-        echo "NEPI Container Running with ID: ${CONTAINER_ID}"
-        echo "--------------------------"   
-        echo ""
-
-        update_yaml_value "NEPI_RUNNING" 1 "$DOCKER_CONFIG_FILE"
-        update_yaml_value "NEPI_RUNNING_FS" "$nepi_fs" "$DOCKER_CONFIG_FILE"
-        update_yaml_value "NEPI_RUNNING_TAG" "$nepi_fs_tag" "${DOCKER_CONFIG_FILE}"
-        update_yaml_value "NEPI_RUNNING_ID" $CONTAINER_ID "${DOCKER_CONFIG_FILE}"
-        update_yaml_value "NEPI_RUNNING_LAUNCH_TIME" "$(date +%Y-%m-%d)" "${DOCKER_CONFIG_FILE}"
-        update_yaml_value "NEPI_FS_RESTART" 0 "${DOCKER_CONFIG_FILE}"
-        update_yaml_value "NEPI_RESTARTING" 0 "${DOCKER_CONFIG_FILE}"
-    fi
-
+    update_yaml_value "NEPI_RUNNING" 1 "$DOCKER_CONFIG_FILE"
+    update_yaml_value "NEPI_RUNNING_FS" "$nepi_fs" "$DOCKER_CONFIG_FILE"
+    update_yaml_value "NEPI_RUNNING_TAG" "$nepi_fs_tag" "${DOCKER_CONFIG_FILE}"
+    update_yaml_value "NEPI_RUNNING_ID" $CONTAINER_ID "${DOCKER_CONFIG_FILE}"
+    update_yaml_value "NEPI_RUNNING_LAUNCH_TIME" "$(date +%Y-%m-%d)" "${DOCKER_CONFIG_FILE}"
+    update_yaml_value "NEPI_FS_RESTART" 0 "${DOCKER_CONFIG_FILE}"
+    update_yaml_value "NEPI_RESTARTING" 0 "${DOCKER_CONFIG_FILE}"
 fi
+
 
