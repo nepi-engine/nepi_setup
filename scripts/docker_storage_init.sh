5377e8ed13620372bbfd1f409d1f9b914baafe2e
nepihost <shobbs@numurus.com>
2026-03-13 10:26:04 -0700

    **

--- scripts/docker_storage_init.sh
+++ scripts/docker_storage_init.sh
@@ -140,7 +140,7 @@ else
                 echo ""
                 echo "Unzipping storage folders from ${storage_latest_zip}"
                 echo ""
-                sudo su unzip -o -q $storage_latest_zip ${CONFIG_USER}
+                sudo unzip -o -q $storage_latest_zip
                 if [ $? -eq 0 ]; then
                     #sudo rm ${storage_latest_zip} > /dev/null 2>&1
                     success_storage=1
