868faa7aaea24c0dbff2aed59fd585ba037ffc48
jseawall@numurus.com <jseawall@numurus.com>
2026-03-25 15:01:03 -0700

    **

--- scripts/docker_env_setup.sh
+++ scripts/docker_env_setup.sh
@@ -463,7 +463,7 @@ sudo apt update
 sudo apt-get install --fix-broken -y
 sudo rm -r ~/.local/share/Trash/info/ 2>/dev/null 
 sudo rm -r ~/.local/share/Trash/files/ 2>/dev/null
-sudo rm -r /tmp/* 2>/dev/null
+#sudo rm -r /tmp/* 2>/dev/null
 sudo rm /var/crash/* 2>/dev/null
 
 
