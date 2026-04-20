724b3651e111e65b169163e75fd99616dcd5030f
Skye Hobbs <shobbs@numurus.com>
2026-04-20 10:19:05 -0700

    **

--- scripts/nepi_docker_init.sh
+++ scripts/nepi_docker_init.sh
@@ -74,6 +74,9 @@ NEPI_ARCH=unknown
 if is_valid_jetson; then
     NEPI_ARCH=jetson
     base_image=ultralytics/ultralytics:latest-jetson-jetpack5
+elif is_valid_rpi; then
+    NEPI_ARCH=arm64
+    base_image=ultralytics/ultralytics:latest-arm64
 elif is_valid_arm64; then
     NEPI_ARCH=arm64
     base_image=ultralytics/ultralytics:latest-arm64
